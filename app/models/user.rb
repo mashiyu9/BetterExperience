9f3cf48c4cfae16ebf8e7d7ebd53b44701c00eb8
yu shimamura <mashiyu0914@gmail.com>
2020-01-30 11:29:01 +0900

    add: deviceの日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :active_block, foreign_key: 'user_id', class_name: 'blacklist', dependent: :destroy
   has_many :passive_block, foreign_key: 'block_user_id', class_name: 'blacklist', dependent: :destroy
