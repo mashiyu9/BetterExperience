ac2ad9f37dd33979a09c91d82e437f790c3918cd
yu shimamura <mashiyu0914@gmail.com>
2020-02-04 04:48:14 +0900

    add: メッセージ機能
    非同期処理の実装

--- app/controllers/game_room_messages_controller.rb
+++ app/controllers/game_room_messages_controller.rb
@@ -2,12 +2,15 @@ class GameRoomMessagesController < ApplicationController
 
   def create
     @message = GameRoomMessage.new(game_room_id: params[:game_room_message][:room_id], write_user_id: current_user.id, body: params[:game_room_message][:body])
-    # @message.save
+    @game_room = GameRoom.find(params[:game_room_message][:room_id])
+    @participants = @game_room.participants
+    @room_message = @game_room.game_room_messages
 
     # redirect_to game_room_path(params[:game_room_message][:room_id])
 
     respond_to do |format|
       if @message.save
+        format.html { redirect_to game_room_path(params[:game_room_message][:room_id])}
         format.js { render :index }
       else
         format.html { redirect_to game_room_path(params[:game_room_message][:room_id]), notice: 'コメントに失敗しました'}
