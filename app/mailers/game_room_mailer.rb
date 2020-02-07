4df935e31bed657032ac09f37eaa0651f284cc99
yu shimamura <mashiyu0914@gmail.com>
2020-02-07 16:00:08 +0900

    add: 自動メール機能の作成

---
+++ app/mailers/game_room_mailer.rb
@@ -0,0 +1,14 @@
+class GameRoomMailer < ApplicationMailer
+  def request_mail(current_user, owner_user)
+    @current_user = current_user
+    @owner_user = owner_user
+    mail to: @owner_user.participant.email, subject: "あなたの募集に参加を希望する人がいます!"
+  end
+
+  def approval_mail(user, room)
+    @user = user
+    @room = room
+
+    mail to: @user.email, subject: "参加要望が承認されました"
+  end
+end
---
+++ test/mailers/previews/game_room_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/game_room_mailer
+class GameRoomMailerPreview < ActionMailer::Preview
+
+end
