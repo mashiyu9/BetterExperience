21d92a7a1ee34fae0abccadac42e6e10e7a43182
yu shimamura <mashiyu0914@gmail.com>
2020-02-10 14:45:10 +0900

    add: game_room new のフロントの作成

--- app/javascript/javascripts/game_room.js
+++ app/javascript/javascripts/game_room.js
@@ -1,4 +1,46 @@
-window.onload = function() {
-  // debugger;
-  $("#test").css("background-color", "red");
-};
+(function($) {
+  $(document).ready(function() {
+    $("#tag_1").on("click", function() {
+      $("#game_title").val("ApexLegends");
+    });
+
+    $("#tag_2").on("click", function() {
+      $("#game_title").val("PUBG");
+    });
+
+    $("#tag_3").on("click", function() {
+      $("#game_title").val("Fortnite");
+    });
+
+    $("#tag_4").on("click", function() {
+      $("#game_title").val("FF14");
+    });
+
+    $("#tag_5").on("click", function() {
+      $("#game_title").val("dead by daylight");
+    });
+
+    $("#tag_6").on("click", function() {
+      $("#game_title").val("ポケモン ソード・シールド");
+    });
+
+    $("#tag_7").on("click", function() {
+      $("#game_title").val("モンスターハンターworld");
+    });
+
+    $("vc_possible").prop("checked");
+
+    $("#vc_possible").change(function() {
+      var val = $("#vc_possible").prop("checked");
+      if (val) {
+        // $("#layout_none").removeClass("layout_none");
+        // $("#layout_none").addClass("layout_block");
+        $("#layout_none").fadeIn(500);
+      } else {
+        // $("#layout_none").removeClass("layout_block");
+        $("#layout_none").fadeOut(100);
+        // $("#layout_none").addClass("layout_none");
+      }
+    });
+  });
+})(jQuery);
